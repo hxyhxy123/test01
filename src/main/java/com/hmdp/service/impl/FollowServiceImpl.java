59d2e14f2b49ce500fb0441d971e0cc09ae87f31
hxy <1392119947@qq.com>
2024-08-02 16:26:34 +0800

    4.0

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -45,7 +45,7 @@ public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> impleme
             //关注 新增数据
             Follow follow=new Follow();
             follow.setFollowUserId(followUserId);
-            System.out.println("asa");
+            System.out.println("a");
             follow.setUserId(id);
             boolean isSuccess = save(follow);
             if (isSuccess){
